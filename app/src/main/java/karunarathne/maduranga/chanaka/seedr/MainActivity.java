77b435c9d45ace60c51de153f44d7232486355c2
kmchmk1026 <kmchmk@gmail.com>
2017-10-14 22:56:21 +0530

    Seedr website is loading. But there is a robot check that needs to be passing.

--- app/src/main/java/karunarathne/maduranga/chanaka/seedr/MainActivity.java
+++ app/src/main/java/karunarathne/maduranga/chanaka/seedr/MainActivity.java
@@ -1,13 +1,111 @@
 package karunarathne.maduranga.chanaka.seedr;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String URL = "www.seedr.cc";
+    private WebView webview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        webview = (WebView) findViewById(R.id.webView);
+        webview.setWebViewClient(new MyBrowser());
+        webview.setWebChromeClient(new MyChromeBrowser());
+        webview.getSettings().setLoadsImagesAutomatically(true);
+        webview.getSettings().setJavaScriptEnabled(true);
+
+        ConnectivityManager con_manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (con_manager.getActiveNetworkInfo() != null
+                && con_manager.getActiveNetworkInfo().isAvailable()
+                && con_manager.getActiveNetworkInfo().isConnected()) {
+            webview.loadUrl("http://" + URL);
+        } else {
+            noInternetError();
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webview.canGoBack()) {
+            webview.goBack();
+        } else {
+            finish();
+        }
+    }
+
+    public void closeApp(View v) {
+        finish();
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+            if (Uri.parse(url).getHost().equals(URL)) {
+                return false;
+            } else {
+                view.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+//                return true;
+                return false;
+            }
+        }
+
+        @Override
+        public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+            noInternetError();
+        }
+    }
+
+    private class MyChromeBrowser extends WebChromeClient {
+        private ProgressDialog mProgress;
+
+        @Override
+        public void onProgressChanged(WebView view, int progress) {
+            if (mProgress == null) {
+                mProgress = new ProgressDialog(MainActivity.this);
+                mProgress.show();
+            }
+            mProgress.setMessage("Loading...");
+            if (progress == 100) {
+                mProgress.dismiss();
+                mProgress = null;
+            }
+        }
+    }
+
+    public void noInternetError() {
+//        webview.loadUrl("file:///android_asset/index.html");
+        AlertDialog.Builder dlgAlert = new AlertDialog.Builder(MainActivity.this);
+        dlgAlert.setTitle("No internet connection!");
+        dlgAlert.setMessage("Please connect to the internet and try again.");
+        dlgAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        dlgAlert.create().show();
     }
 }
